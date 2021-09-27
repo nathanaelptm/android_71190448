8ca82e7bcf5d53c80eb6f354144ec09d6971da27
nathanaelptm <89833174+nathanaelptm@users.noreply.github.com>
2021-09-27 19:34:25 +0700

    pertemuan4

---
+++ pertemuan4_71190448/app/src/main/java/com/example/pertemuan4_71190448/Login.kt
@@ -0,0 +1,11 @@
+package com.example.pertemuan4_71190448
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
